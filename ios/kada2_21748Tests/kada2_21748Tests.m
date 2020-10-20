9164c869b95a2bab802a2af568fc24a1c8c45b30
Crowdbotics <team@crowdbotics.com>
2020-10-20 14:50:31 +0000

    Initial commit.

---
+++ ios/kada2_21748Tests/kada2_21748Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface kada2_21748 : XCTestCase
+
+@end
+
+@implementation kada2_21748
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
